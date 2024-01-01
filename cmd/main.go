75e84d88833681876c033cc4c899c5b733b59dd0
Matheus Trindade <trindadematheus27@gmail.com>
2024-01-01 12:59:54 -0300

    returning server status

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"net"
 	"serverstatus/grpc/pb"
+	"time"
 
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
 	"google.golang.org/grpc"
 )
 
@@ -13,17 +17,25 @@ type Server struct {
 }
 
 func (s *Server) GetServerStatus(ctx context.Context, in *pb.Empty) (*pb.ServerStatusResponse, error) {
+	cpuPercentage, err := cpu.Percent(time.Second, false)
+	diskUsage, err := disk.Usage("/")
+	memUsage, err := mem.VirtualMemory()
+
+	if err != nil {
+		panic(err)
+	}
+
 	return &pb.ServerStatusResponse{
 		DiskUsage: &pb.DiskUsage{
-			UsedSpace:  0,
-			TotalSpace: 1,
+			UsedSpace:  float32(diskUsage.Used),
+			TotalSpace: float32(diskUsage.Total),
 		},
 		CpuUsage: &pb.CPUUsage{
-			UsagePercentage: 0,
+			UsagePercentage: float32(cpuPercentage[0]),
 		},
 		MemoryUsage: &pb.MemoryUsage{
-			UsedMemory:  0,
-			TotalMemory: 0,
+			UsedMemory:  float32(memUsage.Used),
+			TotalMemory: float32(memUsage.Total),
 		},
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module serverstatus/grpc
 go 1.21.1
 
 require (
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
+	github.com/tklauser/go-sysconf v0.3.13 // indirect
+	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/net v0.16.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
 	google.golang.org/grpc v1.60.1 // indirect
