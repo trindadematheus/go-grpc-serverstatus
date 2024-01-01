ff20af5899b02a30053bd1f8463590d896a452e5
Matheus Trindade <trindadematheus27@gmail.com>
2024-01-01 12:28:25 -0300

    grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"context"
+	"net"
 	"serverstatus/grpc/pb"
+
+	"google.golang.org/grpc"
 )
 
 type Server struct {
 	pb.UnsafeServerStatusServer
 }
 
-// func (s *server) GetServerStatus(ctx context.Context, in *pb.Empty) (*pb.ServerStatusResponse, error) {
-// 	return &pb.HelloReply{Message: "Hello again " + in.GetName()}, nil
-// }
+func (s *Server) GetServerStatus(ctx context.Context, in *pb.Empty) (*pb.ServerStatusResponse, error) {
+	return &pb.ServerStatusResponse{
+		DiskUsage: &pb.DiskUsage{
+			UsedSpace:  0,
+			TotalSpace: 1,
+		},
+		CpuUsage: &pb.CPUUsage{
+			UsagePercentage: 0,
+		},
+		MemoryUsage: &pb.MemoryUsage{
+			UsedMemory:  0,
+			TotalMemory: 0,
+		},
+	}, nil
+}
 
 func main() {
-	println("Teste")
+	listener, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	println("Server started")
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterServerStatusServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
 }
